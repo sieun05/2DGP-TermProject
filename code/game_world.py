f390e051adc04efb5ad8bdc3e9388c553fb28e2f
OhSieun <sieun125@tukorea.ac.kr>
2025-11-10 23:17:06 +0900

    바운딩박스 정의, 그리기 / y기준 정렬 코드 위치 변경 (오브젝트 추가됐을 때 정렬)

--- code/game_world.py
+++ code/game_world.py
@@ -1,24 +1,29 @@
 world = [[], [], []] # layers for game objects
+collision_pairs = {} # dictionary
 
 def add_object(o, depth):
     world[depth].append(o)
 
+    for layer in world:
+        layer.sort(key=lambda obj: obj.y, reverse=True)
+
 def add_objects(ol, depth):
     world[depth] += ol
+
+    for layer in world:
+        layer.sort(key=lambda obj: obj.y, reverse=True)
 
 def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
             return
 
-    raise Exception("World 에 존재하지 않는 오브젝트를 지우려고 시도함")
+    raise ValueError('Cannot delete non existing object')
 
 def update():
     for layer in world:
-        # 여기부분 layer 안에 object들의 y값을 기준으로 내림차순 정렬
-        layer.sort(key=lambda obj: obj.y, reverse=True)
-
         for o in layer:
             o.update()
 
@@ -29,5 +34,44 @@ def render():
 
 
 def clear():
+    global world
+
     for layer in world:
         layer.clear()
+
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+def add_collision_pair(key, a, b):
+    if key not in collision_pairs:
+        collision_pairs[key] = [[], []]  # [list of a, list of b]
+    if a:
+        collision_pairs[key][0].append(a)
+    if b:
+        collision_pairs[key][1].append(b)
+
+def handle_collisions():
+    # 등록된 모든 충돌 그룹에 대해 충돌 검사 수행
+
+    for key, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(key, b)
+                    b.handle_collision(key, a)
