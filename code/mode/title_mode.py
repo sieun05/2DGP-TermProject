60a40db7c83c3eb3e9e2a9fea6b69780d671aaa1
OhSieun <sieun125@tukorea.ac.kr>
2025-12-03 16:24:33 +0900

    타이틀모드

--- code/mode/title_mode.py
+++ code/mode/title_mode.py
@@ -6,24 +6,77 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import game_framework
 from . import lobby_mode
 
-image = None
+image_back, image_pc, image_zom, image_font = None, None, None, None
+timer_pc, timer_zom, timer_font = 0.0, 0.0, 0.0
+pc_up, zom_up, font_up = True, True, True
+
+image_pc_y, image_zom_y, image_font_y = 300, 300, 300
 
 def init():
-    global image
-    image = load_image('images/title.png')
+    global image_back, image_pc, image_zom, image_font
+    image_back = load_image('images/title_back.png')
+    image_pc = load_image('images/title_pc.png')
+    image_zom = load_image('images/title_zom.png')
+    image_font = load_image('images/title_font.png')
     logo_start_time = get_time()
+
+    global timer_pc, timer_zom, timer_font
+    timer_pc = timer_zom = timer_font = get_time()
 
 
 def finish():
-    global image
-    del image
+    global image_back, image_pc, image_zom, image_font
+    del image_back, image_pc, image_zom, image_font
 
 def update():
-    pass
+    global timer_pc, timer_zom, timer_font
+
+    if get_time() - timer_pc > 0.05:
+        timer_pc = get_time()
+
+        global pc_up, image_pc_y
+        if pc_up:
+            image_pc_y += 1
+            if image_pc_y >= 300:
+                pc_up = False
+        else:
+            image_pc_y -= 1
+            if image_pc_y <= 280:
+                pc_up = True
+
+    if get_time() - timer_zom > 0.05:
+        timer_zom = get_time()
+
+        global zom_up, image_zom_y
+        if zom_up:
+            image_zom_y += 2
+            if image_zom_y >= 300:
+                zom_up = False
+        else:
+            image_zom_y -= 2
+            if image_zom_y <= 270:
+                zom_up = True
+
+    if get_time() - timer_font > 0.05:
+        timer_font = get_time()
+
+        global font_up, image_font_y
+        if font_up:
+            image_font_y += 1
+            if image_font_y >= 320:
+                font_up = False
+        else:
+            image_font_y -= 1
+            if image_font_y <= 290:
+                font_up = True
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+
+    image_back.draw(400, 300)
+    image_pc.draw(400, image_pc_y)
+    image_zom.draw(400, image_zom_y)
+    image_font.draw(400, 300)
     update_canvas()
 
 def handle_events():
