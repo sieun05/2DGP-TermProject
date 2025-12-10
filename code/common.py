e287e22ffd1c44dd4ad6a368a470c05012f4707a
OhSieun <sieun125@tukorea.ac.kr>
2025-12-10 13:36:45 +0900

    player ui

---
+++ code/common.py
@@ -0,0 +1,10 @@
+from player_ui import *
+
+apple = 0
+gear = 0
+player_max_heart = 100
+player_heart = 100
+player_attack_speed = 1.0
+player_attack_power = 10
+round = 1
+
---
+++ code/player_ui.py
@@ -0,0 +1,10 @@
+from pico2d import *
+
+class PlayerUI:
+    def __init__(self):
+        self.image = load_image('images/player_ui.png')
+        self.player_health_image = load_image('images/player_heart.png')
+        pass
+
+    def draw(self):
+        pass
