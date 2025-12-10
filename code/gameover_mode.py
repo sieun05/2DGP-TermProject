e8ea59fde83a8c8b88d0bc2a43b966cd03eadc15
OhSieun <sieun125@tukorea.ac.kr>
2025-12-10 19:12:42 +0900

    점수 출력

--- code/gameover_mode.py
+++ code/gameover_mode.py
@@ -2,16 +2,19 @@ from pico2d import *
 
 import game_framework
 import title_mode
+import common
 
 image = None
+font = None
 
 def init():
-    global image
+    global image, font
     image = load_image('images/gameover.png')
+    font = load_font('images/ENCR10B.TTF', 50)
 
 def finish():
-    global image
-    del image
+    global image, font
+    del image, font
 
 def update():
     pass
@@ -19,6 +22,10 @@ def update():
 def draw():
     clear_canvas()
     image.draw(400, 300)
+    score = ((common.round + common.level) * 1000 +
+             (common.player_max_heart + common.player_attack_power + common.player_attack_speed) * 20 +
+             (common.apple + common.gear) * 10)
+    font.draw(200, 250, f'score: {int(score)}', (0, 0, 0))
     update_canvas()
 
 #이벤트 변경해야함
--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 from pico2d import *
 import game_framework
-import logo_mode
+import logo_mode, gameover_mode
 
 open_canvas()
-game_framework.run(logo_mode)
+game_framework.run(gameover_mode)
 close_canvas()
