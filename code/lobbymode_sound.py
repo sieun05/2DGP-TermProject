f16013692737d86406a394214018809aad46f7c6
OhSieun <sieun125@tukorea.ac.kr>
2025-12-10 17:43:13 +0900

    loggy_mode bgm 볼륨 조절

--- code/lobbymode_sound.py
+++ code/lobbymode_sound.py
@@ -4,7 +4,7 @@ from sdl2 import *
 class BGM_Lobby:
     def __init__(self):
         self.bgm = load_music('sounds/lobby.mp3')
-        self.bgm.set_volume(32)
+        self.bgm.set_volume(40)
         self.bgm.play()
 
         self.y = 0
